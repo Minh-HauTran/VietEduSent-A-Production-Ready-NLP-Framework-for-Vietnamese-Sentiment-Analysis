852c58c5e06d889af7cb2a45c54b4e293cc703b5
Minh-Hau Tran <tranminhhau9a2thcsttlp@gmail.com>
2026-04-06 15:43:15 +0700

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,15 @@
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
+def compute_metrics(y_true, y_pred):
+    acc = accuracy_score(y_true, y_pred)
+
+    precision, recall, f1, _ = precision_recall_fscore_support(
+        y_true, y_pred, average="weighted"
+    )
+
+    return {
+        "accuracy": acc,
+        "precision": precision,
+        "recall": recall,
+        "f1": f1
+    }
