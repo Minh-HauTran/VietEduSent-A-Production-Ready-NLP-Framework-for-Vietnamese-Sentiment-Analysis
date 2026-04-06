08884a8b988031fe71530989b556df42f7657e02
Minh-Hau Tran <tranminhhau9a2thcsttlp@gmail.com>
2026-04-06 15:44:09 +0700

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,16 +1,16 @@
 import json
 import os
 
-def log_results(results, filename="results/logs.json"):
+def log_results(result, path="results/logs.json"):
     os.makedirs("results", exist_ok=True)
 
-    if os.path.exists(filename):
-        with open(filename, "r") as f:
+    if os.path.exists(path):
+        with open(path, "r") as f:
             data = json.load(f)
     else:
         data = []
 
-    data.append(results)
+    data.append(result)
 
-    with open(filename, "w") as f:
+    with open(path, "w") as f:
         json.dump(data, f, indent=4)
